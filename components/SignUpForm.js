5d447761fc8a61d75eb4647e60ccb51c238a53a2
Trinath Sai Subhash Reddy Pittala <subhash8111999@gmail.com>
2025-05-27 16:04:56 -0400

    setting up firebase auth structure

--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -1,84 +1,93 @@
-import React, { useCallback, useReducer } from "react";
-import Input from "../components/Input";
-import SubmitButton from "../components/SubmitButton";
-import { Feather, FontAwesome } from "@expo/vector-icons";
+import React, { useCallback, useReducer } from 'react';
+import Input from '../components/Input';
+import SubmitButton from '../components/SubmitButton';
+import { Feather, FontAwesome } from '@expo/vector-icons';
 
-import { validateInput } from "../utils/actions/formActions";
-import { reducer } from "../utils/reducers/formReducer";
+import { validateInput } from '../utils/actions/formActions';
+import { reducer } from '../utils/reducers/formReducer';
+import { signUp } from '../utils/actions/authActions';
 
 const initialState = {
-  inputValidities: {
-    firstName: false,
-    lastName: false,
-    email: false,
-    password: false,
-  },
-  formIsValid: false,
-};
-
-const SignUpForm = (props) => {
-  const [formState, dispatchFormState] = useReducer(reducer, initialState);
-
-  const inputChangedHandler = useCallback(
-    (inputId, inputValue) => {
-      const result = validateInput(inputId, inputValue);
-      dispatchFormState({ inputId, validationResult: result });
+    inputValues: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
     },
-    [dispatchFormState]
-  );
+    inputValidities: {
+        firstName: false,
+        lastName: false,
+        email: false,
+        password: false,
+    },
+    formIsValid: false
+}
 
-  return (
-    <>
-      <Input
-        id="firstName"
-        label="First name"
-        icon="user-o"
-        iconPack={FontAwesome}
-        onInputChanged={inputChangedHandler}
-        autoCapitalize="none"
-        errorText={formState.inputValidities["firstName"]}
-      />
+const SignUpForm = props => {
 
-      <Input
-        id="lastName"
-        label="Last name"
-        icon="user-o"
-        iconPack={FontAwesome}
-        onInputChanged={inputChangedHandler}
-        autoCapitalize="none"
-        errorText={formState.inputValidities["lastName"]}
-      />
+    const [formState, dispatchFormState] = useReducer(reducer, initialState);
 
-      <Input
-        id="email"
-        label="Email"
-        icon="mail"
-        iconPack={Feather}
-        onInputChanged={inputChangedHandler}
-        keyboardType="email-address"
-        autoCapitalize="none"
-        errorText={formState.inputValidities["email"]}
-      />
+    const inputChangedHandler = useCallback((inputId, inputValue) => {
+        const result = validateInput(inputId, inputValue);
+        dispatchFormState({ inputId, validationResult: result, inputValue })
+    }, [dispatchFormState]);
 
-      <Input
-        id="password"
-        label="Password"
-        icon="lock"
-        autoCapitalize="none"
-        secureTextEntry
-        iconPack={Feather}
-        onInputChanged={inputChangedHandler}
-        errorText={formState.inputValidities["password"]}
-      />
+    const authHandler = () => {
+        signUp(
+            formState.inputValues.firstName,
+            formState.inputValues.lastName,
+            formState.inputValues.email,
+            formState.inputValues.password,
+        );
+    }
 
-      <SubmitButton
-        title="Sign up"
-        onPress={() => console.log("Button pressed")}
-        style={{ marginTop: 20 }}
-        disabled={!formState.formIsValid}
-      />
-    </>
-  );
+    return (
+            <>
+                <Input
+                    id="firstName"
+                    label="First name"
+                    icon="user-o"
+                    iconPack={FontAwesome}
+                    onInputChanged={inputChangedHandler}
+                    autoCapitalize="none"
+                    errorText={formState.inputValidities["firstName"]} />
+
+                <Input
+                    id="lastName"
+                    label="Last name"
+                    icon="user-o"
+                    iconPack={FontAwesome}
+                    onInputChanged={inputChangedHandler}
+                    autoCapitalize="none"
+                    errorText={formState.inputValidities["lastName"]} />
+
+                <Input
+                    id="email"
+                    label="Email"
+                    icon="mail"
+                    iconPack={Feather}
+                    onInputChanged={inputChangedHandler}
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                    errorText={formState.inputValidities["email"]} />
+
+                <Input
+                    id="password"
+                    label="Password"
+                    icon="lock"
+                    autoCapitalize="none"
+                    secureTextEntry
+                    iconPack={Feather}
+                    onInputChanged={inputChangedHandler}
+                    errorText={formState.inputValidities["password"]} />
+                
+                <SubmitButton
+                    title="Sign up"
+                    onPress={authHandler}
+                    style={{ marginTop: 20 }}
+                    disabled={!formState.formIsValid}/>
+            </>
+    )
 };
 
 export default SignUpForm;
---
+++ utils/actions/authActions.js
@@ -0,0 +1,14 @@
+import { getFirebaseApp } from '../firebaseHelper';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+
+export const signUp = async (firstName, lastName, email, password) => {
+    const app = getFirebaseApp();
+    const auth = getAuth(app);
+
+    try {
+        const result = await createUserWithEmailAndPassword(auth, email, password);
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+}
